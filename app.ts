09679102cf2899287cf2d77315649b5e310da03a
Alejandro3101 <alejo001vale@gmail.com>
2025-04-14 22:02:05 -0500

    agregando proyecto typeScript

---
+++ app.ts
@@ -0,0 +1,14 @@
+console.log('Inicio de programa');
+
+let isTired = true;
+
+if (isTired === true) {
+    console.log('Estoy cansado');
+} else {
+    console.log('Estoy alerta');
+}
+
+
+
+
+console.log('Fin de programa');
---
+++ sections/basic-ty.ts
@@ -0,0 +1,11 @@
+export let firstName: string = 'Alejandro';
+let lastName: string  = 'Valderrama';
+let age: number = 25;
+let isDeveloper: boolean = true;
+let fullName: string = `${firstName} ${lastName}`;
+
+isDeveloper = !!false;
+
+console.log('isDeveloper', isDeveloper);
+
+
